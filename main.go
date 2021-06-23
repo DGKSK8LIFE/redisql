abab20b84b1d403c51ffa38f1cc6e2ae43589fca
DGKSK8LIFE <tarekali15@outlook.com>
2021-06-23 00:07:23 -0700

    fixed formatting

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 /*
 Flag format:
-	go run main.go migrate --sql=localhost:3306 --redis=6379 --options={"uuid": true, TTL: 120}
+	go run main.go migrate --sql=localhost:3306 --redis=6379 --options={"uuid": true, "ttl": 120}
 */
 
 func init() {
