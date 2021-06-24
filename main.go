25745403c5ede0c57bf2d68a5dab812ed730af5e
DGKSK8LIFE <tarekali15@outlook.com>
2021-06-23 20:22:05 -0700

    migrate flag

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 /*
 Flag format:
-	go run main.go migrate --sql=localhost:3306 --redis=6379 --options={"uuid": true, "ttl": 120}
+	go run main.go -migrate --sql=localhost:3306 --redis=6379 --options={"uuid": true, "ttl": 120}
 */
 
 func init() {
+	migrate := flag.Bool("migrate", false, "migrate from mysql to redis")
+	flag.Parse()
 
+	if *migrate {
+		fmt.Println(*migrate)
+	}
 }
 
 func main() {
