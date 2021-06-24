dc50d851aaeed3cf560d9154f3de014de148213b
DGKSK8LIFE <tarekali15@outlook.com>
2021-06-23 20:36:24 -0700

    working flag variables; now will make subflags for seperate commands

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
 )
 
 /*
@@ -11,15 +10,13 @@ Flag format:
 	go run main.go migrate --sql=localhost:3306 --redis=6379
 */
 
-var sql string
-var redis string
+var sql int
+var redis int
 
 func init() {
-	if os.Args[0] == "migrate" {
-		flag.StringVar(&sql, "sql", "localhost:3306", "sql port #")
-		flag.StringVar(&redis, "redis", "localhost:6379", "redis port #")
-		flag.Parse()
-	}
+	flag.IntVar(&sql, "sql", 3306, "sql port")
+	flag.IntVar(&redis, "redis", 6379, "redis port")
+	flag.Parse()
 }
 
 func main() {
