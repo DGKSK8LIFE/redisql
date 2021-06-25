fa0e8a21432ea3eee123a0595774779fb0dacd3d
Tarek Ali <tarekali15@outlook.com>
2021-06-24 22:18:50 -0500

    var names

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module redis-sql
 go 1.13
 
 require (
-	github.com/go-redis/redis/v8 v8.10.0 // indirect
+	github.com/go-redis/redis/v8 v8.10.0
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 )
 
 /*
@@ -10,15 +9,14 @@ Current CLI flag options:
 	go run main.go -sql 3306 -redis 6379
 */
 
-var sql int
-var redis int
+var sqlPort int
+var redisPort int
 
 func init() {
-	flag.IntVar(&sql, "sql", 3306, "sql port")
-	flag.IntVar(&redis, "redis", 6379, "redis port")
+	flag.IntVar(&sqlPort, "sql", 3306, "sql port")
+	flag.IntVar(&redisPort, "redis", 6379, "redis port")
 	flag.Parse()
 }
 
 func main() {
-	fmt.Printf("sql: %d\nredis: %d\n", sql, redis)
 }
