35d15c1adfd359a741101271e9072538f64d71db
Tarek Ali <tarekali15@outlook.com>
2021-06-24 22:29:11 -0500

    redis connection init; tomorrow will fix and add everything

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.13
 
 require (
 	github.com/go-redis/redis/v8 v8.10.0
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
 )
--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+
+	"github.com/go-redis/redis/v8"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 /*
@@ -9,14 +12,19 @@ Current CLI flag options:
 	go run main.go -sql 3306 -redis 6379
 */
 
-var sqlPort int
-var redisPort int
+var sqlPort string
+var redisPort string
 
 func init() {
-	flag.IntVar(&sqlPort, "sql", 3306, "sql port")
-	flag.IntVar(&redisPort, "redis", 6379, "redis port")
+	flag.StringVar(&sqlPort, "sql", "localhost:3306", "sql port")
+	flag.StringVar(&redisPort, "redis", "localhost:6379", "redis port")
 	flag.Parse()
 }
 
 func main() {
+	redis.NewClient(&redis.Options{
+		Addr:     redisPort,
+		Password: "",
+		DB:       0,
+	})
 }
