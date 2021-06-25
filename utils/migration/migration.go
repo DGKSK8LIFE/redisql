9592b5ed2e8b5f9bd9cf2c6104221173ddd1dfdd
Tarek Ali <tarekali15@outlook.com>
2021-06-25 12:09:20 -0500

    reading rows pretest; commented out redis init for now

--- utils/migration/migration.go
+++ utils/migration/migration.go
@@ -2,8 +2,8 @@ package migration
 
 import (
 	"database/sql"
+	"fmt"
 
-	"github.com/go-redis/redis/v8"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -12,17 +12,38 @@ type Celebrity struct {
 	Age  uint8  `sql:"age"`
 }
 
-
 // Migrate takes an SQL table's rows and converts them into Redis hashes
 func Migrate() error {
 	db, err := sql.Open("mysql", "root:password@/celebrities")
 	if err != nil {
 		return err
 	}
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
+
+	rows, err := db.Query(`SELECT * FROM celebrity;`)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	var celebrities []Celebrity
+	for rows.Next() {
+		var name string
+		var age uint8
+		err := rows.Scan(&name, &age)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(name, age)
+		celebrities = append(celebrities, Celebrity{Name: name, Age: age})
+	}
+	if err := rows.Err(); err != nil {
+		return err
+	}
+	// rdb := redis.NewClient(&redis.Options{
+	// 	Addr:     "localhost:6379",
+	// 	Password: "",
+	// 	DB:       0,
+	// })
 	return nil
 }
