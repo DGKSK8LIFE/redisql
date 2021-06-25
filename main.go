026b7b4804de6b57812e670da3bf0cc73cc6de2d
Tarek Ali <tarekali15@outlook.com>
2021-06-25 10:11:46 -0500

    removing CLI for now to test migrations

--- main.go
+++ main.go
@@ -1,30 +1,19 @@
 package main
 
-import (
-	"flag"
-
-	"github.com/go-redis/redis/v8"
-	_ "github.com/go-sql-driver/mysql"
-)
-
 /*
 Current CLI flag options:
 	go run main.go -sql 3306 -redis 6379
 */
 
-var sqlPort string
-var redisPort string
+// var sqlPort string
+// var redisPort string
 
 func init() {
-	flag.StringVar(&sqlPort, "sql", "localhost:3306", "sql port")
-	flag.StringVar(&redisPort, "redis", "localhost:6379", "redis port")
-	flag.Parse()
+	// flag.StringVar(&sqlPort, "sql", "localhost:3306", "sql port")
+	// flag.StringVar(&redisPort, "redis", "localhost:6379", "redis port")
+	// flag.Parse()
 }
 
 func main() {
-	redis.NewClient(&redis.Options{
-		Addr:     redisPort,
-		Password: "",
-		DB:       0,
-	})
+
 }
