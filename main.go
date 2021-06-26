3b4a11d8cb55b1465bd2d0fdf608b59d41706b6b
Tarek Ali <tarekali15@outlook.com>
2021-06-26 17:19:25 -0500

    removed commented-out CLI for now

--- main.go
+++ main.go
@@ -4,20 +4,6 @@ import (
 	"redis-sql/utils/migration"
 )
 
-/*
-Current CLI flag options:
-	go run main.go -sql 3306 -redis 6379
-*/
-
-// var sqlPort string
-// var redisPort string
-
-// func init() {
-// 	// flag.StringVar(&sqlPort, "sql", "localhost:3306", "sql port")
-// 	// flag.StringVar(&redisPort, "redis", "localhost:6379", "redis port")
-// 	// flag.Parse()
-// }
-
 func main() {
 	err := migration.Migrate("root", "celebrities", "celebrity")
 	if err != nil {
