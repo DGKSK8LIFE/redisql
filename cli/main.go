52272077d2075dc7d34fbe643c92700b23f30fb9
Tarek Ali <tarekali15@outlook.com>
2021-07-02 12:25:28 -0500

    added more fields for redis configuration

--- cli/main.go
+++ cli/main.go
@@ -10,6 +10,8 @@ var user *string
 var password *string
 var database *string
 var table *string
+var redisAddr *string
+var redisPass *string
 
 func init() {
 	migrateFlag := flag.NewFlagSet("migrate", flag.ExitOnError)
@@ -17,11 +19,13 @@ func init() {
 	password = migrateFlag.String("password", "", "MySQL password")
 	database = migrateFlag.String("database", "", "MySQL database")
 	table = migrateFlag.String("table", "", "MySQL table")
+	redisAddr = migrateFlag.String("redisaddr", "", "Redis address")
+	redisPass = migrateFlag.String("redispass", "", "Redis password")
 	migrateFlag.Parse(os.Args[2:])
 }
 
 func main() {
-	err := migration.Migrate(*user, *password, *database, *table)
+	err := migration.Migrate(*user, *password, *database, *table, *redisAddr, *redisPass)
 	if err != nil {
 		panic(err)
 	}
