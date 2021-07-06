00f7f4ee33e0f9fe5ccb12168bc45c38b5356c3d
Tarek Ali <tarekali15@outlook.com>
2021-07-06 13:02:32 -0500

    ported CLI to use config struct

--- redisql/main.go
+++ redisql/main.go
@@ -7,26 +7,29 @@ import (
 	redisql "github.com/DGKSK8LIFE/redisql"
 )
 
-var user *string
-var password *string
-var database *string
-var table *string
-var redisAddr *string
-var redisPass *string
+var config redisql.Config
 
 func init() {
 	migrateFlag := flag.NewFlagSet("migrate", flag.ExitOnError)
-	user = migrateFlag.String("user", "root", "MySQL user")
-	password = migrateFlag.String("password", "", "MySQL password")
-	database = migrateFlag.String("database", "", "MySQL database")
-	table = migrateFlag.String("table", "", "MySQL table")
-	redisAddr = migrateFlag.String("redisaddr", "", "Redis address")
-	redisPass = migrateFlag.String("redispass", "", "Redis password")
+	user := migrateFlag.String("user", "root", "MySQL user")
+	password := migrateFlag.String("password", "", "MySQL password")
+	database := migrateFlag.String("database", "", "MySQL database")
+	table := migrateFlag.String("table", "", "MySQL table")
+	redisAddr := migrateFlag.String("redisaddr", "", "Redis address")
+	redisPass := migrateFlag.String("redispass", "", "Redis password")
 	migrateFlag.Parse(os.Args[2:])
+	config = redisql.Config{
+		User:      *user,
+		Password:  *password,
+		Database:  *database,
+		Table:     *table,
+		RedisAddr: *redisAddr,
+		RedisPass: *redisPass,
+	}
 }
 
 func main() {
-	err := redisql.Migrate(*user, *password, *database, *table, *redisAddr, *redisPass)
+	err := config.Migrate()
 	if err != nil {
 		panic(err)
 	}
