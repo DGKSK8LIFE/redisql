fb8930121aa1f3696ddfe02fe65a7937ec568533
Tarek Ali <tarekali15@outlook.com>
2021-07-06 13:36:40 -0500

    consolidated utils files

--- utils/utils.go
+++ utils/utils.go
@@ -3,7 +3,19 @@ package utils
 import (
 	"database/sql"
 	"fmt"
+
+	"github.com/go-redis/redis/v8"
 )
+
+// OpenRedis opens a redis connection with a desired address and password
+func OpenRedis(redisAddress, redisPassword string) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Password: redisPassword,
+		DB:       0,
+	})
+	return rdb
+}
 
 // OpenSQL opens a MySQL connection with a desired user, password, and database name
 func OpenSQL(user, password, database string) (*sql.DB, error) {
