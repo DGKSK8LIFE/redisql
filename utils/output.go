f98acd7f9f7a2fc9250c6ee27b9578cb2e16dded
DGKSK8LIFE <tarekali15@outlook.com>
2021-07-07 21:46:59 -0700

    jank way of formatting; going to redo this

--- utils/output.go
+++ utils/output.go
@@ -4,8 +4,9 @@ import "fmt"
 
 // PrintRow prettyprints a SQL row
 func PrintRow(id string, m map[string]string) {
-	fmt.Println(fmt.Sprintf("%s: \n", id))
+	fmt.Printf("%s: \n\n", id)
 	for key, value := range m {
 		fmt.Printf("\t%s: %s\n", key, value)
 	}
+	fmt.Println()
 }
