b7f13d0e6a7d7b2c686b3108a1cb553b69d9bfa4
DGKSK8LIFE <tarekali15@outlook.com>
2021-07-16 12:42:55 -0500

    added list case to PrintKey switch statement

--- utils/output.go
+++ utils/output.go
@@ -5,14 +5,19 @@ import "fmt"
 // PrintKey prints a desired key based off of the inferred type
 func PrintKey(id string, m interface{}) {
 	switch m.(type) {
+	case string:
+		fmt.Printf("%s: \n\n", id)
+		fmt.Printf("\t%s\n", m)
+	case []string:
+		fmt.Printf("%s: \n\n", id)
+		for _, value := range m.([]string) {
+			fmt.Printf("\t%s\n", value)
+		}
 	case map[string]string:
 		fmt.Printf("%s: \n\n", id)
 		for key, value := range m.(map[string]string) {
 			fmt.Printf("\t%s: %s\n", key, value)
 		}
-	case string:
-		fmt.Printf("%s: \n\n", id)
-		fmt.Printf("\t%s\n", m)
 	}
 	fmt.Println()
 }
