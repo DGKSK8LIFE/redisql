f3bfe48e7b3337c38feb72c380ebd00c3dcfb6cb
DGKSK8LIFE <tarekali15@outlook.com>
2021-07-18 22:36:18 -0700

    removed extra colon from key printing

--- utils/output.go
+++ utils/output.go
@@ -6,15 +6,15 @@ import "fmt"
 func PrintKey(id string, m interface{}) {
 	switch m.(type) {
 	case string:
-		fmt.Printf("%s: \n\n", id)
+		fmt.Printf("%s \n\n", id)
 		fmt.Printf("\t%s\n", m)
 	case []string:
-		fmt.Printf("%s: \n\n", id)
+		fmt.Printf("%s \n\n", id)
 		for _, value := range m.([]string) {
 			fmt.Printf("\t%s\n", value)
 		}
 	case map[string]string:
-		fmt.Printf("%s: \n\n", id)
+		fmt.Printf("%s \n\n", id)
 		for key, value := range m.(map[string]string) {
 			fmt.Printf("\t%s: %s\n", key, value)
 		}
