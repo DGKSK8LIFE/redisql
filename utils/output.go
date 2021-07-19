b3aeab3bdd2c827c3e25a1df655eb74740116a27
DGKSK8LIFE <tarekali15@outlook.com>
2021-07-18 22:38:23 -0700

    shortened + simplified PrintKey

--- utils/output.go
+++ utils/output.go
@@ -4,17 +4,15 @@ import "fmt"
 
 // PrintKey prints a desired key and its values based off of the inferred type
 func PrintKey(id string, m interface{}) {
+	fmt.Printf("%s \n\n", id)
 	switch m.(type) {
 	case string:
-		fmt.Printf("%s \n\n", id)
 		fmt.Printf("\t%s\n", m)
 	case []string:
-		fmt.Printf("%s \n\n", id)
 		for _, value := range m.([]string) {
 			fmt.Printf("\t%s\n", value)
 		}
 	case map[string]string:
-		fmt.Printf("%s \n\n", id)
 		for key, value := range m.(map[string]string) {
 			fmt.Printf("\t%s: %s\n", key, value)
 		}
