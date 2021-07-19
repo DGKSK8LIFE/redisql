1b32235fadc93e3ab8e92fb1619aa100ccaa620c
DGKSK8LIFE <tarekali15@outlook.com>
2021-07-18 23:37:35 -0700

    simplified devex of copy.go so much...db.go still needs a lot of work; pretest

--- copy.go
+++ copy.go
@@ -3,6 +3,7 @@ package redisql
 import (
 	"context"
 
+	"github.com/DGKSK8LIFE/redisql/utils"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -21,12 +22,24 @@ var ctx = context.Background()
 
 // CopyToHash reads a desired SQL table's rows and writes them to Redis hashes
 func (c Config) CopyToHash() error {
+	if err := utils.Convert("hash", c.SQLUser, c.SQLPassword, c.SQLDatabase, c.SQLTable, c.RedisAddr, c.RedisPass, c.Log); err != nil {
+		return err
+	}
+	return nil
 }
 
 // CopyToString reads a desired SQL table's rows and writes them to Redis strings
 func (c Config) CopyToString() error {
+	if err := utils.Convert("string", c.SQLUser, c.SQLPassword, c.SQLDatabase, c.SQLTable, c.RedisAddr, c.RedisPass, c.Log); err != nil {
+		return err
+	}
+	return nil
 }
 
 // CopyToList reads a desired SQL table's rows and writes them to Redis lists
 func (c Config) CopyToList() error {
+	if err := utils.Convert("list", c.SQLUser, c.SQLPassword, c.SQLDatabase, c.SQLTable, c.RedisAddr, c.RedisPass, c.Log); err != nil {
+		return err
+	}
+	return nil
 }
