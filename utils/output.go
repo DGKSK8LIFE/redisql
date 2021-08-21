ed4e625ce43214dc4d5c99b8dacf25f9125addc8
Tarek Ali <tarekali15@outlook.com>
2021-08-21 15:50:32 -0700

    reverted to previous print system; found out issue was with wrongtype operation when trying to overwrite list with hset; mb

--- utils/output.go
+++ utils/output.go
@@ -4,18 +4,18 @@ import "fmt"
 
 // printKey prints a desired key and its values based off of the inferred type
 func printKey(id string, m interface{}) {
-	fmt.Printf("%s ➝️ ", id)
+	fmt.Printf("%s \n\n", id)
 	switch m.(type) {
 	case string:
-		fmt.Printf("%s", m)
+		fmt.Printf("\t%s\n", m)
 	case []string:
 		for _, value := range m.([]string) {
-			fmt.Printf("%s, ", value)
+			fmt.Printf("\t%s\n", value)
 		}
 	case map[string]string:
 		for key, value := range m.(map[string]string) {
-			fmt.Printf("%s: %s,", key, value)
+			fmt.Printf("\t%s: %s\n", key, value)
 		}
 	}
-	fmt.Printf("\n")
+	fmt.Println()
 }
