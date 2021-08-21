87e03435f324e79461589e7004c29db35990f77a
Tarek Ali <tarekali15@outlook.com>
2021-08-21 14:51:49 -0700

    better formatting for list logging

--- utils/output.go
+++ utils/output.go
@@ -10,7 +10,7 @@ func printKey(id string, m interface{}) {
 		fmt.Printf("%s", m)
 	case []string:
 		for _, value := range m.([]string) {
-			fmt.Printf("%s...", value)
+			fmt.Printf("%s, ", value)
 		}
 	case map[string]string:
 		for key, value := range m.(map[string]string) {
