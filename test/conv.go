ff6cb2a16fe550de61b5d37ce777570a80ca89dc
Tarek Ali <tarekali15@outlook.com>
2021-08-29 10:16:59 -0700

    docs for TestCopyToString

--- test/conv.go
+++ test/conv.go
@@ -7,6 +7,14 @@ import (
 )
 
 func TestCopyToString(t *testing.T) {
+	/*
+		- Needs to create database, and table schema from `schema.sql`
+		- Then inserts random or preset data 1,000,000 times (1 million table rows)
+		- Then runs CopyToString() on the table
+		- Goals:
+			1. Benchmark various Copy() functions
+			2. Look for edgecases and major issues in them
+	*/
 	config := redisql.Config{
 		SQLUser:     "root",
 		SQLPassword: "password",
