b5f3070c50cd1b5abb944a5f0650b4d432e691ca
Tarek Ali <tarekali15@outlook.com>
2021-09-03 21:52:28 -0700

    doing simpler, more streamlined test

--- copy_test.go
+++ copy_test.go
@@ -8,14 +8,6 @@ import (
 )
 
 func TestCopyToString(t *testing.T) {
-	/*
-		- Needs to create database, and table schema from `schema.sql`
-		- Then inserts random or preset data 1,000,000 times (1 million table rows)
-		- Then runs CopyToString() on the table
-		- Goals:
-			1. Benchmark various Copy() functions
-			2. Look for edgecases and major issues in them
-	*/
 	config := Config{
 		SQLUser:     "root",
 		SQLPassword: "password",
@@ -31,32 +23,27 @@ func TestCopyToString(t *testing.T) {
 		t.Error(err)
 		t.Fail()
 	}
-	// _, err = db.Exec(`CREATE DATABASE IF NOT EXISTS ?`, config.SQLDatabase)
-	// if err != nil {
-	// 	t.Error(err)
-	// 	t.Fail()
-	// }
-	// _, err = db.Exec(`
-	// CREATE TABLE IF NOT EXISTS user (
-	// 	id INT AUTO_INCREMENT PRIMARY KEY,
-	// 	name VARCHAR(255) NOT NULL,
-	// 	uuid VARCHAR(64) NOT NULL,
-	// 	height VARCHAR(5) NOT NULL,
-	// 	shoesize TINYINT NOT NULL,
-	// 	age TINYINT NOT NULL,
-	// 	bio TEXT NOT NULL,
-	// 	friends_count TINYINT NOT NULL,
-	// 	favorite_animal VARCHAR(20) NOT NULL,
-	// 	favorite_color VARCHAR(10) NOT NULL,
-	// 	favorite_food VARCHAR(20) NOT NULL,
-	// 	mobile_phone VARCHAR(50) NOT NULL
-	// );
-	// `)
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS user (
+		id INT AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(255) NOT NULL,
+		uuid VARCHAR(64) NOT NULL,
+		height VARCHAR(5) NOT NULL,
+		shoesize TINYINT NOT NULL,
+		age TINYINT NOT NULL,
+		bio TEXT NOT NULL,
+		friends_count TINYINT NOT NULL,
+		favorite_animal VARCHAR(20) NOT NULL,
+		favorite_color VARCHAR(10) NOT NULL,
+		favorite_food VARCHAR(20) NOT NULL,
+		mobile_phone VARCHAR(50) NOT NULL
+	);
+	`)
 	if err != nil {
 		t.Error(err)
 		t.Fail()
 	}
-	for i := 0; i < 100000; i++ {
+	for i := 0; i < 1000; i++ {
 		_, err = db.Exec(`INSERT INTO user VALUES (NULL, "martin", "f8d1c837-719f-42a9-9a37-0e2ed7c0e458",  "5'9", "9", 15, "Student and Developer", 100, "horse", "red", "apple", "555-555-5555")`)
 		if err != nil {
 			t.Error(err)
