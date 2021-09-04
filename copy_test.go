94dbbad8bea5d95e63b4395079eb77a5f1bfe985
Tarek Ali <tarekali15@outlook.com>
2021-09-04 16:31:10 -0700

    refactoring tests to add support for all convert metohds

--- copy_test.go
+++ copy_test.go
@@ -1,29 +1,14 @@
 package redisql
 
 import (
-	"os"
+	"context"
+	"fmt"
 	"testing"
 
 	"github.com/DGKSK8LIFE/redisql/utils"
 )
 
-func TestCopyToString(t *testing.T) {
-	config := Config{
-		SQLUser:     "root",
-		SQLPassword: "password",
-		SQLDatabase: "users",
-		SQLTable:    "user",
-		RedisAddr:   "localhost:6379",
-		RedisPass:   "",
-		Log:         false,
-	}
-	db, err := utils.OpenSQL(config.SQLUser, config.SQLPassword, config.SQLDatabase)
-	defer db.Close()
-	if err != nil {
-		t.Error(err)
-		t.Fail()
-	}
-	_, err = db.Exec(`
+var insertString = `
 	CREATE TABLE IF NOT EXISTS user (
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		name VARCHAR(255) NOT NULL,
@@ -38,25 +23,67 @@ func TestCopyToString(t *testing.T) {
 		favorite_food VARCHAR(20) NOT NULL,
 		mobile_phone VARCHAR(50) NOT NULL
 	)
-	`)
+`
+var config Config
+
+var ctx context.Context
+
+func PrepareTest() {
+	fmt.Println("Preparing Test...")
+	config = Config{
+		SQLUser:     "root",
+		SQLPassword: "password",
+		SQLDatabase: "users",
+		SQLTable:    "user",
+		RedisAddr:   "localhost:6379",
+		RedisPass:   "",
+		Log:         false,
+	}
+	db, err := utils.OpenSQL(config.SQLUser, config.SQLPassword, config.SQLDatabase)
+	defer db.Close()
 	if err != nil {
-		t.Error(err)
-		t.Fail()
+		panic(err)
+	}
+	rdb := utils.OpenRedis(config.RedisAddr, config.RedisPass)
+	defer rdb.Close()
+	_, err = db.Exec(insertString)
+	if err != nil {
+		panic(err)
 	}
 	_, err = db.Exec(`DELETE FROM user`)
 	if err != nil {
-		t.Error(err)
-		t.Fail()
+		panic(err)
 	}
+	rdb.FlushDB(ctx)
 	for i := 0; i < 1000; i++ {
 		_, err = db.Exec(`INSERT INTO user VALUES (NULL, "martin", "f8d1c837-719f-42a9-9a37-0e2ed7c0e458",  "5'9", "9", 15, "Student and Developer", 100, "horse", "red", "apple", "555-555-5555")`)
 		if err != nil {
-			t.Error(err)
-			t.Fail()
-			os.Exit(1)
+			panic(err)
 		}
 	}
-	err = config.CopyToString()
+}
+
+func TestCopyToString(t *testing.T) {
+	fmt.Println("Testing CopyToString...")
+	err := config.CopyToString()
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+}
+
+func TestCopyToList(t *testing.T) {
+	fmt.Println("Testing CopyToList...")
+	err := config.CopyToList()
+	if err != nil {
+		t.Error(err)
+		t.Fail()
+	}
+}
+
+func TestCopyToHash(t *testing.T) {
+	fmt.Println("Testing CopyToHash...")
+	err := config.CopyToHash()
 	if err != nil {
 		t.Error(err)
 		t.Fail()
