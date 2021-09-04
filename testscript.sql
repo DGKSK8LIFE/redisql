9b77e657597aa8b68ac034c0907108a559998e55
Tarek Ali <tarekali15@outlook.com>
2021-09-03 21:05:42 -0700

    proper formatting

--- testscript.sql
+++ testscript.sql
@@ -14,27 +14,30 @@ CREATE TABLE IF NOT EXISTS user (
     favorite_food VARCHAR(20) NOT NULL,
     mobile_phone VARCHAR(50) NOT NULL
 );
-delimiter $$ CREATE PROCEDURE insertLoop() BEGIN 
-DECLARE i INT DEFAULT 1;
-WHILE (i <= 100000) DO
-INSERT INTO user
-VALUES (
-        NULL,
-        "martin",
-        "f8d1c837-719f-42a9-9a37-0e2ed7c0e458",
-        "5'9",
-        "9",
-        15,
-        "Student and Developer",
-        100,
-        "horse",
-        "red",
-        "apple",
-        "555-555-5555"
-    );
-SET i = i + 1;
-END WHILE;
-END $$
+CREATE PROCEDURE insertLoop() 
+BEGIN
+    START TRANSACTION;    
+    DECLARE i INT DEFAULT 1;
+    WHILE (i <= 100000) DO
+        INSERT INTO user
+        VALUES (
+            NULL,
+            "martin",
+            "f8d1c837-719f-42a9-9a37-0e2ed7c0e458",
+            "5'9",
+            "9",
+            15,
+            "Student and Developer",
+            100,
+            "horse",
+            "red",
+            "apple",
+            "555-555-5555"
+        );
+        SET i = i + 1;
+    END WHILE;
+    COMMIT; 
+END;
 /* 
  Notes: use transaction for insertion then commit once it is done for more optimal performance (MySQL)
  */
